2bc6449bbcc4bbc90fdaa0f663141324539018e1
esotka <eriksotka@gmail.com>
2020-11-06 10:17:12 -0500

    A commit from my local computer

--- R.phenotype/field.experiment.analysis.R
+++ R.phenotype/field.experiment.analysis.R
@@ -1,9 +1,11 @@
-### Field experiment - ANOVAs
+### Field experiment - published ANOVAs
 library(lmerTest)
 library(reshape)
 rm(list=ls())
 dat <- read.csv("data.phenotype/tall.short_fieldexp_100617_removebiomassoutliers.csv",header=TRUE)
 dat <- dat[dat$Survival.Yes==1,] # missing plots are removed.
+
+stats1 <- c()
 
 y <- c("max.Height","Average.Height","num.live.stems","Total.biomass","Aboveground.Biomass..g.","Belowground.Biomass..g.","Above.below")
 for(i in 1:length(y))
@@ -13,9 +15,30 @@ tmp <- dat[,c("Origin.Zone","Transplant.Zone","Transplant.Site","Origin.Site","P
 tmp$y <- dat[,colnames(dat)==as.character(y[i])]
 tmp <- tmp[!is.na(tmp$y),]
 tmp$y <- as.numeric(as.character(tmp$y))
-m <- lmer(y~Transplant.Zone*Origin.Zone + (1|Plot../Transect) + (1|Transplant.Site) +  (1|Origin.Site),data=tmp)
+m <- lmer(y~Transplant.Zone*Origin.Zone + (1|Transplant.Site) +  (1|Origin.Site),data=tmp)
 print(anova(m))
+stats1 <- rbind(stats1,data.frame(y=y[i],x=c("Transplant.Zone","Origin.Zone","Interaction"),p=round(anova(m)$'Pr(>F)',3)))
 }
+
+### Field experiment - ANOVAs using siteXzone as "home vs away"
+dat$Origin.Zone.Site <- factor(paste(dat$Origin.Zone,dat$Origin.Site,sep="."))
+dat$Transplant.Zone.Site <- factor(paste(dat$Transplant.Zone,dat$Transplant.Site,sep="."))
+stats2 <- c()
+for(i in 1:length(y))
+{
+  print(paste("response =",y[i]))
+  tmp <- dat[,c("Origin.Zone.Site","Transplant.Zone.Site","Transplant.Site","Origin.Site","Plot..","Transect")]
+  tmp$y <- dat[,colnames(dat)==as.character(y[i])]
+  tmp <- tmp[!is.na(tmp$y),]
+  tmp$y <- as.numeric(as.character(tmp$y))
+  tmp$Origin.Zone.Site <- factor(tmp$Origin.Zone.Site)
+  tmp$Transplant.Zone.Site <- factor(tmp$Transplant.Zone.Site)
+  m <- lm(y~Transplant.Zone.Site*Origin.Zone.Site ,data=tmp)
+  print(anova(m))
+  stats2 <- rbind(stats2,data.frame(y=y[i],x=c("Transplant.Zone.Site","Origin.Zone.Site","Interaction"),p=round(anova(m)$'Pr(>F)'[1:3],3)))
+}
+
+print(cbind(stats1,stats2[,2:3]))
 
 ### Field experiment - survival
 
